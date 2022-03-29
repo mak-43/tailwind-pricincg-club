6b08fb4f5accb5059f7b00c7a7a7baa9d8cdba49
Mak-43 <letsmailmak@gmail.com>
2022-03-29 17:51:10 +0600

    menu bar

--- src/Components/Link/Link.js
+++ src/Components/Link/Link.js
@@ -4,7 +4,7 @@ const Link = (props) => {
     const {name,link}=props.route
     return (
         
-            <li><a href={link}>{name}</a></li>
+            <li className='mr-12'><a href={link}>{name}</a></li>
        
     );
 };
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '../Link/Link';
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
 
 const Navbar = () => {
+    const [open,setOpen]=useState(false)
     const routes=[
         {id:1,name:'Home',link:'/home'},
         {id:2,name:'Shop',link:'/shop'},
@@ -11,7 +13,10 @@ const Navbar = () => {
     ]
     return (
        <nav>
-           <ul>
+           <div onClick={()=>setOpen(!open)} className='w-5 h-5 md:hidden'>
+               {open?<XIcon></XIcon> :<MenuIcon></MenuIcon>}
+           </div>
+           <ul className={`md:flex justify-center absolute duration-500 ease-in ${open? 'top-6':'top-[-120px]'}`}>
                {
                    routes.map(route=><Link 
                    key={route.id}
